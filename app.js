e35d6f96d8b2ce4d5e859db36a2e059894b03261
TABISH-FARHAN <148621063+TABISH-FARHAN@users.noreply.github.com>
2024-01-17 09:48:42 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,571 @@
+
+
+
+// var firstname = " hello tabish"
+// alert("first name:"  + firstname)
+
+// var firstName = "John";
+// alert("First name: " + firstName);
+
+
+
+// var lastName = "Doe";
+// alert("Last name: " + lastName);
+
+
+
+// var email = "john.doe@example.com";
+// alert("Email: " + email);
+
+
+
+// var phoneNumber = "+1 555-1234";
+// alert("Phone number: " + phoneNumber);
+
+
+
+
+// alert("You are learning JavaScript");
+
+
+
+// alert("i like pancakes")
+
+// alert("asa mat karo meri jan ")
+
+
+
+// var myVariable = "This is a variable in camelCase format";
+
+
+
+// // Declare a variable without defining it
+// var myVariable;
+
+// // Assign a string to the variable in a second statement
+// myVariable = "This is a string of your choice";
+
+// // Now, you can use the variable
+// alert(myVariable);
+
+
+// var a = ['abc' , 'kjl' , 'yut' , 'abc' , 'mno' , 'xyz']
+// for (var i = 0;  10; i++)
+// console.log(a(i))
+// for (var i =0; i > 10; i++){console.log(a[1])};
+
+// var a = ['abc' , 'kjl' , 'yut' , 'abc' , 'mno' , 'xyz']
+// console.log(a(i))
+// if (a[i]=="mno"){console.log(word)}
+
+
+
+
+// var age = 20;
+
+// var studentcard = true;
+
+// if (age>18 || studentcard ==true)
+
+// {console.log ("allow")}
+
+
+// var age = 20
+
+// var nic = false
+// if (age>9 || nic ==false )
+// {console.log("not allow")}
+
+
+// var a = ['abc' , 'kjl' , 'yut' , 'abc' , 'mno' , 'xyz']
+// var b = ['1' , '2' , '3' , '4']
+
+// var i = a.indexof ('xyz')
+
+// if (i =-1)
+// {console.log('word found')}
+// var count = a.length
+// console.log(count) 
+
+// var a = "karachi";
+// var b = prompt("enter city name")
+
+// if (a == b){
+//     console.log("city matched")
+// }
+
+
+
+
+// var b = prompt("enter city name")
+// b = b.toLowerCase();
+
+
+// if (a == b)  {
+//     console.log("city matched")
+// }
+
+// var arr = ['KARACHI' , 'MULTAN' , 'LAHORE' , 'QUETTA'];
+// var a = prompt("ENTER CITY NAME");
+
+
+// for (var i = 0; i<arr.length; i++) {
+//     console.log (arr(i))
+//     arr[1] = arr[i].toLowerCase()
+//     console.log(arr[i])
+
+
+
+// }
+
+
+// var a = "karachi";
+// var letter1 = a.slice(0, 1);
+// letter1 = letter1.toUpperCase;
+
+
+
+// var otherletters = a.slice(1);
+// otherletters = otherletters.toLowerCase;
+// var word = letter1 + otherletters;
+// console.log(word);
+
+
+
+
+// var a = "ffdgnrtgftnetnetnetne rnter te4 nt  tg berge treghnrghe terngrt erterg gerg drgebger geterefr reggtrdg fte  tte ret wer erse r ee  rest yyjfdfzawgfcdzsazxcevfghhyrftttttttttttttttttttttttttttt 5twer4teer ererfer er  er erererere r"
+
+
+
+
+// var word = "tg";
+// var secondword = "fte";
+// var startingtext;
+// var endingtext;
+// for (var i = 0; i < a.length; i++) {        
+//     console.log(i);
+//     var checkletter = a.slice(i, i + 8);
+//     console.log(checkletter);
+//     if (checkletter == word) {
+//         console.log ("word found")
+
+//         var startingtext = a.slice (0, 1);
+//         var endingtext = a.slice (1);       
+//     console.log(startingtext);   
+//         console.log(endingtext);
+
+
+//     }
+// }
+
+
+
+
+// var a = 3.6;
+// var b = Math.round(a);
+// console.log(b);
+
+
+
+// var a = 30.3;
+// var b = Math.ceil(a);
+// console.log(b);
+
+
+
+
+// var a = 80.9;
+// var b = Math.floor(a);
+// console.log(b) ;
+
+
+
+
+// var a = Math.random();
+// console.log(a);
+
+// var a = Math.random();
+// var b = Math.round(a);
+// console.log(a,b);
+
+
+
+
+
+
+
+// // var a = "100.99";
+// // var b = parseInt(a)
+// // console.log(b + 33);
+
+
+
+// // var a = 100 / 3 ;
+// // var b = a.toFixed(a);
+// // console.log(b);
+
+// // var a = 100.64;
+// // var b = a.toString( );
+// // console.log(b);
+
+// // var a = 648.3
+// // console.log(a * 2000);
+
+// // var b = a.toFixed(2);
+// // console.log(b * 2000);
+
+
+// // var currentDate = new Date("2023-12-01T12:30:50");
+
+
+
+// // console.log(currentDate.getDate());
+
+// // console.log(currentDate.getMonth());
+// // console.log(currentDate.getTime());
+// // console.log(currentDate.getMinutes());
+// // console.log(currentDate.getSeconds());
+// // console.log(currentDate.getDay());
+// // console.log(currentDate.getFullYear());
+
+// // var a  = 300;
+// // var b  = 0.4345345;
+// // var c  = (a * b) / 15;
+
+// // console.log(c);
+
+
+// // var a = new Date ();
+// // console.log(a);
+
+
+// // a.getDate()
+
+// // console.log(a.getDate())
+
+// // 1st jan 1970
+
+// // a.setDate()
+// 
+
+// var a = new Date()
+
+// a.setDate ((new Date()).getDate()-10)
+// console.log(a);
+
+// var a = '2.9'
+// var b = parseInt(a)
+// console.log(b)
+
+
+// function abc () {
+
+
+// alert("ABC");
+
+
+// console.log("ABC")
+// var a = 10
+// console.log(a);
+// }
+
+// abc ()
+
+
+
+
+// function abc(a , b){
+//     var amount = a - b;
+// console.log(amount);
+
+
+// }
+
+
+// abc(250 , 1000);
+// abc(1000 , 300);
+// abc(400 , 100);
+// abc(500 , 5000);
+
+
+
+// var a;
+
+
+// function abc(){
+//     a = 10
+// }
+
+// abc ();
+// console.log(a);
+
+// var a = 10;
+
+// function calcDiscount(price , discount) {
+// var amount = price - discount 
+// return amount 
+// }
+// var a = calcDiscount(10000 , 5000)
+
+// console.log(a)
+
+
+// function calAge(a) {
+//     var todayDate = new Date(a)
+//     var Date  = Date(30-11-2004)
+//      var age = todayDate - Date
+//     console.log(age)
+
+// } 
+
+
+// function abc() {
+//     return "Tabish"
+// }
+
+
+// var a  = abc();
+// console.log(a);
+
+
+
+// function numReverse(x){
+//     var a = x.toString().split("").reverse().join("");
+//     console.log(a)
+// }
+
+// numReverse(34223)
+
+
+
+
+
+
+
+
+// function abc (x){
+// var a = x.toString().split("").reverse().join("")
+// console.log(a)
+// }
+
+// abc ("madam")
+// abc ("nurses run")
+
+
+
+
+// var a = 10; ------------> global variable
+// var a = 10;
+
+// function calcDiscount(price, discount) {
+//     var amount = price - discount;
+//     return amount;
+//     console.log(amount);
+// }
+
+// var b = calcDiscount(500 ,50);
+// console.log(b);
+
+// function abc() {
+//     return "abc12345678";
+// }
+
+// var a = abc();
+// console.log(a);
+
+
+
+// function calAge(date){
+//     let todayDate = new Date();
+//     //console.log(todayDate);
+//     let currentYear = todayDate.getFullYear(); 
+//     //console.log(currentYear);
+//     let year = date.slice(5);
+//     let currentAge = currentYear - year ;
+//     console.log(currentAge);
+       
+//    }
+//    calAge("23-09-2005");
+
+
+// function abc(x){
+//     var x = abc.reverse();
+//     return out;
+// }
+
+// abc(243);
+
+
+// function numReverse(x) {
+//     var a = x.toString().split("").reverse().join("");
+//     return a;
+// }
+
+// console.log(numReverse(123456789));
+
+
+// function checkpalindrome(value){
+//     var arrayvalues = value.split("");
+//     var reversearrayvaluse = arrayvalues.reverse().join("");
+//     if (value === reversearrayvaluse){
+//         return "it is a Palindrome"
+//     }
+//     else {
+//         return "it is not Palindrome"
+//     }
+// }
+
+// console.log(checkpalindrome("non"))
+
+
+// function upperCase(value){
+//     var value = value.upperCase("");
+// }
+
+
+// function capitalizeWords(str) {
+//     const words = str.split(' ');
+//     const capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+//     const result = capitalizedWords.join(' ');
+
+//     return result;
+// }
+// const inputString = 'the quick brown fox';
+// console.log(capitalizeWords(inputString));
+
+
+
+
+function abc(text){
+var a = text.split(" ")
+console.log(a)
+
+for (var i = 0; i < a.length ; i++)
+a[i]}
+
+
+// Write a function which tells the presense of (word) in an array = ['zaid','haris','raza','abubakar','hassan','hussain','fatima']
+// result should be in true or false
+
+
+
+// Google Method --------------------------------------
+
+function Greatest_Lowest(arr_num) {
+    arr_num.sort(function(x, y) {
+      return x - y;
+    });
+    var uniqa = [arr_num[0]];
+    var result = [];
+    for (var j = 0; j < arr_num.length; j++) {
+      if (arr_num[j - 1] !== arr_num[j]) {
+        uniqa.push(arr_num[j]);
+      }
+    }
+    result.push(uniqa[0], uniqa[uniqa.length - 1]);
+    return result.join(',');
+  }
+  console.log(Greatest_Lowest([1, 2, 3, 4, 5])); 
+
+
+  let dObj = new Date();
+
+  let dStr = new Date().toString();
+
+  let day = d.getDay();
+  let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  alert(dayNames[d]);
+
+  let fullDate = new Date();
+  let year = fullDate.getFullYear();
+  let month = fullDate.getMonth();
+  let Day = fullDate.getDate();
+  let hours = fullDate.getHours();
+  let minutes = fullDate.getMinutes();
+  let seconds = fullDate.getSeconds();
+  let milliseconds = fullDate.getMilliseconds();
+
+
+
+  let later = new Date(2020, 11, 31);
+
+  let referenceDate = new Date(0); // January 1, 1970
+  let beginning1980 = new Date(1980, 0, 1);
+  let millisecondsElapsed = beginning1980.getTime() - referenceDate.getTime();
+  alert(millisecondsElapsed);
+
+
+  let date = new Date();
+  date.setFullYear(2022); // Change the year to 2022
+
+
+
+
+  function changeMonthToJanuary(date) {
+    date.setMonth(0); // January is represented by index 0
+  }
+  
+  // Example usage
+  let myDate = new Date();
+  changeMonthToJanuary(myDate);
+
+
+
+  function changeMinutes(time, newMinutes) {
+    time.setMinutes(newMinutes);
+  }
+  
+  // Example usage
+  let time = new Date();
+  let newMinutes = prompt("Enter the new minutes value:");
+  changeMinutes(myTime, newMinutes);
+
+
+  function addHours(time, hoursToAdd) {
+    time.setHours(time.getHours() + hoursToAdd);
+  }
+  
+  // Example usage
+  let myTime = new Date();
+  let hoursToAdd = 3;
+  addHours(myTime, hoursToAdd);
+
+
+
+
+
+  function calculateAge(birthdate) {
+    let currentDate = new Date();
+    let birthdateObj = new Date(birthdate);
+    
+    let age = currentDate.getFullYear() - birthdateObj.getFullYear();
+    if (currentDate.getMonth() < birthdateObj.getMonth() || (currentDate.getMonth() === birthdateObj.getMonth() && currentDate.getDate() < birthdateObj.getDate())) {
+      age--;
+    }
+    
+    return age;
+  }
+  
+  // Example usage
+  let birthdate = "1990-05-15";
+  let age = calculateAge(birthdate);
+  console.log(age); // Output: 31 (assuming current date is in 2021)
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
